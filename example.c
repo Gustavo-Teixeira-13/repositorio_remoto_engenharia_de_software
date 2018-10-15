89704511950a7665199bfa81e1edd8a8d14eaa7e
Gustavo Teixeira <21736@ufp.edu.pt>
2018-10-15 01:15:31 +0100

    mudancas no example.c(novo ciclo for) e no ola.txt(nova linha infinita a dizer ola).

--- example.c
+++ example.c
@@ -4,7 +4,15 @@ int main(int argc, const char *argv[]){
 
 	printf("Hello World!\n");
 	
-	printf("Ola mundo!\n"\);
+	printf("Ola mundo!\n");
+
+	int i=0;
+
+	for(i=0;i<3;i++){
+		
+		printf("%d\n",i);
+
+	}
 
 	return 0;
 
